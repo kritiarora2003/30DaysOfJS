f6e0aadd096ff113dd8dfabc3a8047aeb8c23e58
kritiarora2003 <kritiarora002@gmail.com>
2024-06-18 14:13:57 +0530

    day 22 exercise 3 part 3

--- day_21/exercise_3/index.js
+++ day_21/exercise_3/index.js
@@ -47,6 +47,72 @@ for (let i = 0; i < challenges.length; i++) {
 }
 
 
+// making the author section
+const author_info = asabenehChallenges2020.author
+let authorName = document.querySelector('.author')
+let authorSocials = document.querySelector('.socials')
+let authorBio = document.querySelector('.bio')
+let skills = document.querySelector('.skills')
+let keywords = document.querySelector('.keywords')
+
+// setting the name 
+authorName.innerHTML = `<h1> ${author_info.firstName} ${author_info.lastName} </h1>`
+
+// setting socials
+for (let i = 0; i < author_info.socialLinks.length; i++) {
+    let socialMedia = document.createElement('span')
+    socialMedia.innerHTML = `
+     		<a href="${author_info.socialLinks[i].url}">
+             ${author_info.socialLinks[i].social}
+			</a>   
+    `
+    authorSocials.appendChild(socialMedia)
+}
+
+// setting bio
+authorBio.textContent = author_info.bio
+
+// setting skills
+let authorTitles = document.createElement('div')
+let authorSkills = document.createElement('div')
+let authorQualifications = document.createElement('div')
+
+authorTitles.innerHTML = `<h3> Titles </h3>`
+authorSkills.innerHTML = `<h3> SKills </h3>`
+authorQualifications.innerHTML = `<h3> Qualifications </h3>`
+
+for (let i = 0; i < author_info.titles.length; i++) {
+    let title = document.createElement('p')
+    title.textContent = author_info.titles[i][0] + author_info.titles[i][1]
+    authorTitles.appendChild(title)
+}
+for (let i = 0; i <  author_info.skills.length; i++) {
+    let skill = document.createElement('p')
+    skill.textContent = author_info.skills[i]
+    authorSkills.appendChild(skill)
+}
+for (let i = 0; i < author_info.qualifications.length; i++) {
+    let element = document.createElement('p')
+    element.textContent = author_info.qualifications[i]
+    authorQualifications.appendChild(element)
+}
+
+skills.appendChild(authorTitles)
+skills.appendChild(authorSkills)
+skills.appendChild(authorQualifications)
+
+
+// setting keywords
+let keyword_header = document.createElement('div')
+keyword_header.classList.add('keyword_header')
+keyword_header.innerHTML = `<h2>Keywords</h2>`
+keywords.appendChild(keyword_header)
+for (let i = 0; i < asabenehChallenges2020.keywords.length; i++) {
+    let item = document.createElement('p')
+    item.textContent = '#'+asabenehChallenges2020.keywords[i]
+    item.style.background = getRandomHexColor()
+    keywords.appendChild(item)
+}
 
 
 
